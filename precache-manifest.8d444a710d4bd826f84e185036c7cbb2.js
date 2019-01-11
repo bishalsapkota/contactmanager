32b3edf9176ff0eee158d91e70c403a3c0a09af1
bishalsapkota <bishalsap2012@gmail.com>
2019-01-11 13:42:46 +0545

    Updates

---
+++ precache-manifest.8d444a710d4bd826f84e185036c7cbb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2b3c29ffc89bba1c38e",
+    "url": "/contactmanager/static/css/main.7c54dd5d.chunk.css"
+  },
+  {
+    "revision": "d2b3c29ffc89bba1c38e",
+    "url": "/contactmanager/static/js/main.d2b3c29f.chunk.js"
+  },
+  {
+    "revision": "63002edd23a7d3934162",
+    "url": "/contactmanager/static/css/1.74b65dec.chunk.css"
+  },
+  {
+    "revision": "63002edd23a7d3934162",
+    "url": "/contactmanager/static/js/1.63002edd.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "c78d1b083abc229ee52f010fe00cc918",
+    "url": "/contactmanager/index.html"
+  }
+];
